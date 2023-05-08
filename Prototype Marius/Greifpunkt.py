a5b7387c0b07e2360a5009fb202bdcc1fabe92dc
mwestbrock <marius.westbrock@outlook.de>
2023-05-08 19:39:11 +0200

    Revert "mmm"
    This reverts commit 929b45d4083f0f31384daefca213dad524fad4c3, reversing
    changes made to 56c1d0bdb43a49872726edee11c795f59bc43789.

--- Prototype Marius/Greifpunkt.py
+++ Prototype Marius/Greifpunkt.py
@@ -1,38 +1,7 @@
 import cv2
 import numpy as np
 
-<<<<<<< HEAD
 def grippingPoint(bounding_box): #bounding_box = Pfad zum Bild bounding_box = Extrahiertes Objekt als Bild
-=======
-
-# Bild laden
-img = cv2.imread('F:/mariu/Desktop/Projekt 3 HS/Einhorn.png')
-
-# Graustufenbild erzeugen
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-# Kantenerkennung mit Canny durchführen
-edges = cv2.Canny(gray, 100, 200)
-
-# Konturen finden
-contours, hierarchy = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-#
-
-# Leeres Bild für Kontur erstellen
-contour_img = np.zeros_like(img)
-
-# Kontur in das Bild zeichnen
-cv2.drawContours(contour_img, contours, -1, (0, 255, 0), 2)
-
-# Konturbild anzeigen
-cv2.imshow('Konturbild', contour_img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-=======
-#Bild einlesen
-image = cv2.imread('C:/Users/mariu/4. Semester/Projekt/Bilder/Katze.png')
->>>>>>> 41c5a51eead10f224544feb8ec2152ac975ff036
 
     image =  cv2.imread(bounding_box)
 
@@ -62,8 +31,4 @@ image = cv2.imread('C:/Users/mariu/4. Semester/Projekt/Bilder/Katze.png')
     cv2.destroyAllWindows()
 
 
-<<<<<<< HEAD
 grippingPoint('C:/Users/mariu/4. Semester/Projekt/Bilder/Einhorn.png')
-=======
->>>>>>> 8cf177c177e5eb87764c2fe824a38dcfc719df92
->>>>>>> 41c5a51eead10f224544feb8ec2152ac975ff036
