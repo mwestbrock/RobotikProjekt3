c295a25e84b881400bab4e361637368e2c3e53f3
mwestbrock <marius.westbrock@outlook.de>
2023-06-06 17:01:45 +0200

    Update imageProcessorAruco.py

--- Aruco/imageProcessorAruco.py
+++ Aruco/imageProcessorAruco.py
@@ -4,23 +4,24 @@ import numpy as np
 
 class ImageProcessor:
     def __init__(self):
-        self.cap = cv2.VideoCapture(0)
         self.aruco_dict = aruco.getPredefinedDictionary(aruco.DICT_4X4_250)
         self.parameters = aruco.DetectorParameters()
-        self.marker_centers = []
+        self.warped_frame = None
 
-    def detect_markers(self):
-        ret, frame = self.cap.read()
-        if not ret:
-            return
+    def resize_frame(self, frame, scale_percent):
+        width = int(frame.shape[1] * scale_percent / 100)
+        height = int(frame.shape[0] * scale_percent / 100)
 
+        return cv2.resize(frame, (width, height), interpolation=cv2.INTER_AREA)
+
+    def detect_markers(self, frame):
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
         detector = aruco.ArucoDetector(self.aruco_dict, detectorParams=self.parameters)
 
         corners, ids, rejected_corners = detector.detectMarkers(gray)
 
-        self.marker_centers = []
+        marker_centers = []
 
         if len(corners) > 0:
             aruco.drawDetectedMarkers(frame, corners, ids)
@@ -28,42 +29,48 @@ class ImageProcessor:
             for i in range(len(ids)):
                 center_x = int((corners[i][0][0][0] + corners[i][0][2][0]) / 2)
                 center_y = int((corners[i][0][0][1] + corners[i][0][2][1]) / 2)
-                self.marker_centers.append((ids[i][0], center_x, center_y))
+                marker_centers.append((ids[i][0], center_x, center_y))
 
         # Sortiere die marker_centers nach aufsteigender ID
-        self.marker_centers = sorted(self.marker_centers, key=lambda x: x[0])
+        marker_centers = sorted(marker_centers, key=lambda x: x[0])
 
-        cv2.imshow("Aruco Detection", frame)
+        return marker_centers
+        #cv2.imshow("Aruco Detection", frame)
 
-        if len(self.marker_centers) >= 4:
-            self.transform_to_birds_eye_view()
 
-    def transform_to_birds_eye_view(self):
-        ret, frame = self.cap.read()
+
+
+
+    def transform_to_birds_eye_view(self, frame, marker_centers):
         height, width, _ = frame.shape
 
-        src_points = np.array((self.marker_centers[1][1:], self.marker_centers[0][1:],
-                               self.marker_centers[2][1:], self.marker_centers[3][1:]), dtype=np.float32)
+        src_points = np.array((marker_centers[2][1:], marker_centers[3][1:],
+                               marker_centers[1][1:], marker_centers[0][1:]), dtype=np.float32)
         dst_points = np.array([[0, 0], [width, 0], [width, height], [0, height]], dtype=np.float32)
         perspective_matrix = cv2.getPerspectiveTransform(src_points, dst_points)
 
         warped_frame = cv2.warpPerspective(frame, perspective_matrix, (width, height))
-        cv2.imshow("Bird's-eye View", warped_frame)
+        return warped_frame
+        #cv2.imshow("Bird's-eye View", self.warped_frame)
+
+    def crop_warped_image(self, warped_frame):
+        if warped_frame is None:
+            return None
+        x = 0
+        y = self.cmToPixel(3)
+        width = warped_frame.shape[1]
+        height = warped_frame.shape[0] - self.cmToPixel(6)
+
+        cropped_frame = warped_frame[y:y+height, x:x+width]
+        #cv2.imshow("Crop_IMG", self.cropped_frame)
+        return cropped_frame
+
+    def cmToPixel(self, cm):
+        # 1cm = 40px
+        return cm * 40
 
     def release(self):
-        self.cap.release()
         cv2.destroyAllWindows()
 
-
-# Usage
-detector = ImageProcessor()
-
-while True:
-    detector.detect_markers()
-
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-detector.release()
-
-print(detector.marker_centers)
+#Beispiel aufruf mit Video
+cap = cv2.VideoCapture("C:/Users/Maximilian/Desktop/Video.mp4")
